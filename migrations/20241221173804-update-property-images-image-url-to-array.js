3118cd4ce6ab07287384384f845625a9c1cde031
aceoflife <teddyogbonnaya@yahoo.com>
2024-12-21 18:53:32 +0100

    image string to array

--- migrations/20241221173804-update-property-images-image-url-to-array.js
+++ migrations/20241221173804-update-property-images-image-url-to-array.js
@@ -2,16 +2,41 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('PropertyImages', 'image_url', {
-      type: Sequelize.ARRAY(Sequelize.STRING),  // Change to array of strings
+    // Add a temporary column with the correct type
+    await queryInterface.addColumn('PropertyImages', 'new_image_url', {
+      type: Sequelize.ARRAY(Sequelize.STRING),
       allowNull: false,
+      defaultValue: [],
     });
+
+    // Copy data from the old `image_url` column to the new column
+    await queryInterface.sequelize.query(
+      `UPDATE "PropertyImages" SET "new_image_url" = ARRAY["image_url"]::text[]`
+    );
+
+    // Remove the old column
+    await queryInterface.removeColumn('PropertyImages', 'image_url');
+
+    // Rename the new column to the original name
+    await queryInterface.renameColumn('PropertyImages', 'new_image_url', 'image_url');
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('PropertyImages', 'image_url', {
-      type: Sequelize.STRING,  // Revert back to a single string
+    // Add the old column back (reverting changes)
+    await queryInterface.addColumn('PropertyImages', 'new_image_url', {
+      type: Sequelize.STRING,
       allowNull: false,
     });
+
+    // Copy data back from the array to the string
+    await queryInterface.sequelize.query(
+      `UPDATE "PropertyImages" SET "new_image_url" = "image_url"[1]`
+    );
+
+    // Remove the array column
+    await queryInterface.removeColumn('PropertyImages', 'image_url');
+
+    // Rename the column back to the original name
+    await queryInterface.renameColumn('PropertyImages', 'new_image_url', 'image_url');
   }
 };
