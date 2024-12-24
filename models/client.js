c8a397fb61583f0c1359787302fa8886d5a8038f
aceoflife <teddyogbonnaya@yahoo.com>
2024-12-24 01:32:01 +0100

    models

--- models/client.js
+++ models/client.js
@@ -1,34 +1,5 @@
-// // models/client.js
-// 'use strict';
-
-// module.exports = (sequelize, DataTypes) => {
-//   const Client = sequelize.define('Client', {
-//     user_id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       references: {
-//         model: 'Users', // Assuming you have a Users table
-//         key: 'id',
-//       },
-//       onUpdate: 'CASCADE',
-//       onDelete: 'CASCADE',
-//     },
-//   }, {});
-
-//   Client.associate = function(models) {
-//     Client.belongsTo(models.User, {
-//       foreignKey: 'user_id',
-//       as: 'user', // This will allow you to access the user details
-//     });
-//     // Other associations can be added as needed
-//   };
-
-//   return Client;
-// };
-
-
 // models/Client.js
-const { DataTypes } = require('sequelize'); // Import DataTypes correctly
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/config'); // Import sequelize instance
 const User = require('./user'); // Import User model
 
