5968b31be5f9cff96cbdd0299fae1e00f884962e
aceoflife <teddyogbonnaya@yahoo.com>
2024-12-24 01:37:49 +0100

    client.js update

--- models/client.js
+++ models/client.js
@@ -1,7 +1,7 @@
 // models/Client.js
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../config/config'); // Import sequelize instance
-const User = require('./user'); // Import User model
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./index');  // Correctly import sequelize from index.js
+const User = require('./user');  // Import User model
 
 const Client = sequelize.define('Client', {
   user_id: {
