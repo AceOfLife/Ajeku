2a878cc2f7a0d6d51c11ab03a1f87fba8cea25ac
aceoflife <teddyogbonnaya@yahoo.com>
2025-02-11 19:13:03 +0100

    Update

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -19,36 +19,36 @@
 //   }
 // };
 
-// exports.updateTransaction = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const [updated] = await Transaction.update(req.body, { where: { id } });
+exports.updateTransaction = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [updated] = await Transaction.update(req.body, { where: { id } });
 
-//     if (updated) {
-//       const updatedTransaction = await Transaction.findOne({ where: { id } });
-//       res.status(200).json(updatedTransaction);
-//     } else {
-//       res.status(404).json({ message: 'Transaction not found' });
-//     }
-//   } catch (error) {
-//     res.status(400).json({ message: 'Error updating transaction', error });
-//   }
-// };
+    if (updated) {
+      const updatedTransaction = await Transaction.findOne({ where: { id } });
+      res.status(200).json(updatedTransaction);
+    } else {
+      res.status(404).json({ message: 'Transaction not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Error updating transaction', error });
+  }
+};
 
-// exports.deleteTransaction = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const deleted = await Transaction.destroy({ where: { id } });
+exports.deleteTransaction = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Transaction.destroy({ where: { id } });
 
-//     if (deleted) {
-//       res.status(204).json({ message: 'Transaction deleted' });
-//     } else {
-//       res.status(404).json({ message: 'Transaction not found' });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error deleting transaction', error });
-//   }
-// };
+    if (deleted) {
+      res.status(204).json({ message: 'Transaction deleted' });
+    } else {
+      res.status(404).json({ message: 'Transaction not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error deleting transaction', error });
+  }
+};
 
 
 // exports.verifyPayment = async (req, res) => {
