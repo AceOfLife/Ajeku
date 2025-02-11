f3191cba9261dda02a0118552ec11e308ae48753
aceoflife <teddyogbonnaya@yahoo.com>
2025-02-11 18:03:36 +0100

    initialize-payment route added to paymentRoutes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { verifyPayment } = require("../controllers/paymentController");
+const { initializePayment, verifyPayment } = require("../controllers/paymentController"); // Ensure both functions are imported
 
 router.get("/verify-payment", verifyPayment);
+router.post("/initialize-payment", initializePayment); // Add this line
 
 module.exports = router;
