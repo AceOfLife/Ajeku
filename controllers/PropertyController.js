8840e68b20e649642ada885d94a0001ce61fe5b0
aceoflife <teddyogbonnaya@yahoo.com>
2025-04-13 03:06:56 +0100

    Rollback

--- controllers/PropertyController.js
+++ controllers/PropertyController.js
@@ -585,15 +585,6 @@ exports.getPropertySlots = async (req, res) => {
       const allOwnerships = await FractionalOwnership.findAll({
         where: { property_id }
       });
-
-      // Map through all purchases and include user details
-      const purchases = allOwnerships.map(ownership => ({
-        user_id: ownership.user_id,
-        user_name: ownership.User.name,
-        user_email: ownership.User.email,
-        slots_purchased: ownership.slots_purchased,
-        purchase_date: ownership.createdAt
-      }));
   
       const totalPurchasedSlots = allOwnerships.reduce(
         (sum, record) => sum + record.slots_purchased,
@@ -605,8 +596,7 @@ exports.getPropertySlots = async (req, res) => {
         name: property.name,
         available_slots: property.fractional_slots,
         total_purchased_slots: totalPurchasedSlots,
-        total_slots: property.fractional_slots + totalPurchasedSlots,
-        purchases
+        total_slots: property.fractional_slots + totalPurchasedSlots
       });
   
     } catch (error) {
