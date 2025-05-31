a94615dbea6147d9dfc3393f950679e4f4574ba1
aceoflife <teddyogbonnaya@yahoo.com>
2025-05-31 11:17:37 +0100

    User Transaction history

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,19 +1,31 @@
-// const express = require('express');
-// const TransactionController = require('../controllers/TransactionController');
-
+// const express = require("express");
 // const router = express.Router();
+// const { createTransaction, getAllTransactions, getTransactionById } = require("../controllers/TransactionController");
 
-// router.post('/initialize-payment', TransactionController.initializePayment);
-// router.get('/verify-payment', TransactionController.verifyPayment);
+// router.post("/create", createTransaction);
+// router.get("/", getAllTransactions);
+// router.get("/:id", getTransactionById);
 
 // module.exports = router;
 
 const express = require("express");
 const router = express.Router();
-const { createTransaction, getAllTransactions, getTransactionById } = require("../controllers/TransactionController");
+const {
+  createTransaction,
+  getAllTransactions,
+  getTransactionById,
+  getUserTransactionHistory,
+} = require("../controllers/TransactionController");
 
+const { authenticate, authorizeAdmin } = require("../middlewares/authMiddleware");
+
+// User's personal transaction history
+router.get("/my-history", authenticate, getUserTransactionHistory);
+
+// Existing routes
 router.post("/create", createTransaction);
-router.get("/", getAllTransactions);
+router.get("/", authorizeAdmin, getAllTransactions); // Assuming only admin can see all
 router.get("/:id", getTransactionById);
 
 module.exports = router;
+
