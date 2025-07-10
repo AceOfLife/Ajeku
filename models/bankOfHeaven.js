c1019acfc39312403ce528913ae35b7a5a55c8b2
aceoflife <teddyogbonnaya@yahoo.com>
2025-07-10 20:10:07 +0100

    bank of heaven

--- models/bankOfHeaven.js
+++ models/bankOfHeaven.js
@@ -35,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         defaultValue: 0.0,
       },
-      expenses_per_week: {
+      expenses_per_month: {
         type: DataTypes.FLOAT,
         allowNull: false,
         defaultValue: 0.0,
